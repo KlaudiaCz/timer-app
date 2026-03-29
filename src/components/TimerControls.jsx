38af2f3eeee443c7b6bdc6d7d9155bc1f3251c8f
Klaudia Czetyrow <czetyrow@gmail.com>
2026-03-29 13:29:36 +0200

    Dodano ustawienie focusa na Start/Pause btn

--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -1,8 +1,21 @@
+import { useEffect, useRef } from "react";
+
 const TimerControls = ({ toogleTimer, resetTimer, isRunning }) => {
-    
+  // `startButtonRef` przechowuje referencję do elementu DOM przycisku, co pozwala nam na bezpośrednią manipulację tym elementem, np. ustawienie focusu po zamontowaniu komponentu.
+  const startButtonRef = useRef(null);
+
+  // a `useEffect` z pustą tablicą zależności uruchamia się tylko raz. Po zamontowaniu komponentu sprawdza, czy referencja do przycisku jest ustawiona, a jeśli tak, to ustawia focus na tym przycisku. 
+  // Dzięki temu użytkownik może od razu zacząć korzystać z funkcji Start/Pause bez konieczności ręcznego klikania w przycisk.
+  useEffect(() => {
+    if (startButtonRef.current) {
+      // Po zamontowaniu komponentu ustawiamy focus na przycisku Start/Pause.
+      startButtonRef.current.focus();
+    }
+  }, []);
   return (
     <div>
       <button
+        ref={startButtonRef}
         onClick={toogleTimer}
         className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
       >
